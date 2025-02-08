2cbaed1742f41f2571b5eb8bfeec8cbe6382cde8
Nidhi Soni <nidhisoni4411@gmail.com>
2025-02-08 21:46:07 +0530

    Test Case 3: Add Two Numbers (Comma-Separated)

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,6 +1,6 @@
 class Calculator < ApplicationRecord
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.to_i
+    numbers.split(",").map(&:to_i).sum
   end
 end
--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe Calculator, type: :model do
     it "returns the number itself when a single number is passed" do
       expect(Calculator.add("1")).to eq(1)
     end
+
+    it "returns the sum of two comma-separated numbers" do
+      expect(Calculator.add("1,5")).to eq(6)
+    end
   end
 end
