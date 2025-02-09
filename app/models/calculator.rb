c43fd08c4a00349753daa01013e86224b003e5ee
Nidhi Soni <nidhisoni4411@gmail.com>
2025-02-09 15:23:22 +0530

    Test Case 6: Support Different Delimiters

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,6 +1,12 @@
 class Calculator < ApplicationRecord
   def self.add(numbers)
-    return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+	return 0 if numbers.empty?
+
+	if numbers.start_with?("//")
+	  delimiter, numbers = numbers[2..].split("\n", 2)
+	  numbers.split(delimiter).map(&:to_i).sum
+	else
+	  numbers.split(/,|\n/).map(&:to_i).sum
+	end
   end
 end
--- spec/models/calculator_spec.rb
+++ spec/models/calculator_spec.rb
@@ -21,5 +21,9 @@ RSpec.describe Calculator, type: :model do
 	it "handles new lines as delimiters" do
 	  expect(Calculator.add("1\n2,3")).to eq(6)
 	end
+
+	it "supports different delimiters" do
+	  expect(Calculator.add("//;\n1;2")).to eq(3)
+	end
   end
 end
